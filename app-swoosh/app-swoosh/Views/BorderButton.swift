2e0199487952820f6d88f2e1be1ff9d08aa6b482
Laurent Pantaloni <lpantaloni@mymacbook.home>
2018-09-06 22:29:50 +0200

    First commit

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Laurent Pantaloni on 05/09/2018.
+//  Copyright © 2018 Laurent Pantaloni. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
